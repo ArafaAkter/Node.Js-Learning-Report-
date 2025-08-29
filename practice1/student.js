331615bc2656daba9a4496ec5492d70f30c213a1
Arafa Akter <arafaakter924@gmail.com>
2025-08-29 09:24:19 +0600

    local module

---
+++ practice1/student.js
@@ -0,0 +1,25 @@
+
+// Local modules
+
+
+const getName = () =>{
+    return "Arafa akter";
+}
+const getAge = () =>{
+    return "23";
+}
+
+// we can also access variable using import and export.
+
+const result = 3.75;
+
+
+// this module export system is not orginized.
+
+exports.getName = getName;
+exports.getAge = getAge;
+exports.result = result;
+
+// we use this procedure
+
+// module.exports ={getName,getAge,result}
